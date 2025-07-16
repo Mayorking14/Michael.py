b9a259e777ba0adb7a3a9f625b157d5b3b8d8e25
Mayorking14 <edwardmichael953@gmail.com>
2025-07-16 15:50:15 +0100

    Add files via upload

---
+++ Chapter 2/arithmetic.py
@@ -0,0 +1,21 @@
+#2.4)
+ #(Arithmetic) For each of the arithmetic operators +, -, *, /, // and **, display the value of an expression with 27.5 as the left operand and 2 as the right operand.
+
+#Ans:
+value = 27.5 + 2
+print('Value is: ' , value)
+
+value = 27.5 - 2
+print('Value is: ' , value)
+
+value = 27.5 * 2
+print('Value is: ' , value)
+
+value = 27.5 / 2
+print('Value is: ' , value)
+
+value = 27.5 // 2
+print('Value is: ' , value)
+
+value = 27.5 ** 2
+print('Value is: ' , value)
---
+++ Chapter 2/space.py
@@ -0,0 +1,8 @@
+#2.11)
+# (Separating the Digits in an Integer) Write a script that inputs a five-digit #integer
+# from the user. Separate the number into its individual digits. Print them #separated by three
+ #spaces each. For example, if the user types in the number 42339, the script # #print 4   2   3   3   9
+
+#Ans:
+space = input('enter five digit: ')
+print("space '',   '',   '',   '',   '', ")
